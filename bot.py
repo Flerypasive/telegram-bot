eb8eaecc6005a64c61f08ee8364080843ffc303f
Flerypasive <flerypasive@gmail.com>
2025-08-08 04:01:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,22 @@
 import telebot
 from flask import Flask, request
 
-API_TOKEN = '8238596626:AAHCPswJ-lyH_Hn5Ec_-CFuYxgyazisEDxM'
-
-bot = telebot.TeleBot(API_TOKEN)
+TOKEN = '8238596626:AAHCPswJ-lyH_Hn5Ec_-CFuYxgyazisEDxM'
+bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
+def start_message(message):
     bot.send_message(message.chat.id, "Илюша, я тебя люблю 💖")
 
-@app.route(f"/{API_TOKEN}", methods=['POST'])
-def webhook():
-    update = telebot.types.Update.de_json(request.stream.read().decode("utf-8"))
-    bot.process_new_updates([update])
-    return "ok", 200
+@app.route(f'/{TOKEN}', methods=['POST'])
+def getMessage():
+    bot.process_new_messages([telebot.types.Update.de_json(request.stream.read().decode("utf-8")).message])
+    return "!", 200
 
-@app.route("/", methods=["GET"])
+@app.route('/')
 def index():
-    return "бот работает", 200
+    return "Бот запущен!", 200
 
 if __name__ == "__main__":
-    # Важно: host='0.0.0.0', иначе Render не увидит сервер
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
